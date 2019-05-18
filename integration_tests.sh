0b133d7f960e45eca8b248a85a022be8dcf312b6
Jens Neuhalfen <jens@neuhalfen.name>
2019-05-18 14:28:37 +0200

    Add integration tests

---
+++ integration_tests.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env sh
+
+eval $(./tst_setup_env.sh) || { echo Failed to setup ENV; exit 1; }
+
+error_count=0
+
+count_error()
+{
+  error_count=$(($error_count + 1))
+}
+
+notmuchtask()
+{
+  pipenv run notmuchtask $*
+}
+
+test_exitcode()
+{
+  expected=$1
+  test_name="$2"
+  echo "$test_name" >&2
+  shift
+  shift
+  $*
+  actual=$?
+  if [ ! $actual -eq $expected ]; then
+    echo "FAILED: $test_name: Expected exit code $expected but got $actual for command '$*'" >&2
+    count_error
+  fi
+}
+
+empty()
+{
+  test_name="$1"
+  shift
+  if [ ! "$1" = "" ]; then
+    echo "FAILED: $test_name: Result should be empty" >&2
+    count_error
+  fi
+}
+
+not_empty()
+{
+  test_name="$1"
+  shift
+  if [ "$1" = "" ]; then
+    echo "FAILED: $test_name: Result should not be empty" >&2
+    count_error
+  fi
+}
+
+same()
+{
+  test_name="$1"
+  shift
+  if [ ! "$1" = "$2" ]; then
+    echo "FAILED: $test_name: Expected '$1' actual '$2'" >&2
+    count_error
+  fi
+}
+
+test_exitcode 0 "help works" notmuchtask --help
+test_exitcode 91 "Correct error for invalid file"  notmuchtask find-task /not/existing
+
+MSG_1=$MAILDIR/sample-mail.txt
+taskid=$(test_exitcode 93 "find-task for message without task is empty" notmuchtask find-task $MSG_1)
+empty "No taskid returned if no task found" "$taskid"
+
+taskid_created=$(test_exitcode 0 "find-or-create-task for message without task gives id" notmuchtask find-or-create-task $MSG_1)
+not_empty "taskid should be returned if task is created" "$taskid_created"
+
+taskid_found=$(test_exitcode 0 "find-task for message with task is returned" notmuchtask find-task $MSG_1)
+same "the same taskid should be returned when searching" $taskid_created $taskid_found
+
+if [ ! $error_count -eq 0 ]; then
+  echo "$error_count failed tests"
+  exit 1
+fi
---
+++ tst_setup_env.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env sh
+
+#
+# Create a completely new setup (Maildir, notmuch DB, pipenv)
+#
+
+
+get_exec()
+{
+   query=$1
+   cmd=$(which $query)
+   if [ "$cmd" = "" ]; then
+     echo "'$query' not in path; aborting" >&2
+     exit 1
+   fi
+
+   if [ ! -x $cmd ]; then
+     echo "'$cmd' not an executable; aborting" >&2
+     exit 1
+   fi
+   echo "Using '$cmd' for '$query'" >&2
+   echo $cmd
+}
+
+notmuch=$(get_exec "notmuch")
+task=$(get_exec "task")
+pipenv=$(get_exec "pipenv")
+mktemp=$(get_exec "mktemp")
+
+#
+# Setup
+#
+
+
+temp_dir=$($mktemp -d)  || { echo failed to create temp. dir; exit 1; }
+
+echo Using $temp_dir >&2
+
+export MAILDIR="${temp_dir}/Mail"
+echo Using MAILDIR $MAILDIR >&2 
+mkdir -p "$MAILDIR" || { echo failed to create mail dir $MAILDIR; exit 1; }
+
+export NOTMUCH_CONFIG="${temp_dir}/notmuch-config"
+echo Using NOTMUCH_CONFIG $NOTMUCH_CONFIG >&2 
+
+cat <<EOF>"${NOTMUCH_CONFIG}"
+# .notmuch-config - Configuration file for the notmuch mail system
+[database]
+path=${MAILDIR}
+
+[user]
+name=Example User
+primary_email=recipent@localhost
+
+[new]
+tags=new
+ignore=.mbsyncstate;.isyncuidmap.db;.uidvalidity
+
+[search]
+exclude_tags=deleted;spam;muted
+
+[maildir]
+synchronize_flags=true
+EOF
+
+cp -r test/example-mails/* "$MAILDIR" || { echo Failed to copy mails to $MAILDIR; exit 1; }
+
+$notmuch new  >&2 || { echo notmuch new failed; exit 1; }
+
+msg_count=0$($notmuch count "Example Message")
+if [ ! $msg_count -gt 0 ]; then
+  echo "Should have found messages - aborting" >&2
+  exit 1
+fi
+
+#
+# Taskwarrior
+#
+
+
+export TASKRC="${temp_dir}/taskrc"
+export TASK_DATA="${temp_dir}/tasks"
+mkdir -p "$TASK_DATA" || { echo failed to create $TASK_DATA; exit 1; }
+
+cat <<EOF>"${TASKRC}"
+data.location=$TASK_DATA
+EOF
+
+task_count=0$($task count)
+if [ ! $task_count -eq 0 ]; then
+  echo "Should have found no tasks - aborting" >&2
+  exit 1
+fi
+
+#
+# Install
+#
+
+export WORKON_HOME="${temp_dir}/venv"
+mkdir -p "$WORKON_HOME" || { echo failed to create $WORKON_HOME; exit 1; }
+$pipenv --python 3.6 --site-packages >&2 || { echo failed to create venv ; exit 1; }
+$pipenv run pip install  --editable .  >&2 || { echo failed to install from source; exit 1; }
+
+# eval this
+echo export WORKON_HOME=$WORKON_HOME
+echo export MAILDIR=$MAILDIR
+echo export NOTMUCH_CONFIG=$NOTMUCH_CONFIG
+echo export TASKRC=$TASKRC
